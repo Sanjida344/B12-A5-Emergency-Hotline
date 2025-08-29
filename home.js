2e37bef7f45746ce44ae67b4d08404c38e0c6631
sanjida344 <sanjidatrisha617@gmail.com>
2025-08-29 11:32:39 +0600

    addad an home.js file

---
+++ home.js
@@ -0,0 +1,15 @@
+
+// Variable declare
+let heartCount = 0;
+let coinCount = 100;
+let copyCount = 0;
+
+const heartDisplay = document.getElementById("heartCount");
+const coinDisplay = document.getElementById("coinCount");
+const copyDisplay = document.getElementById("copyCount");
+const historyList = document.getElementById("historyList");
+const clearButton = document.querySelector(".clear-history");
+
+const heartButtons = document.querySelectorAll(".heart-btn");
+const copyButtons = document.querySelectorAll(".copy-btn");
+const callButtons = document.querySelectorAll(".call-btn");
